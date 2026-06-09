e607f4c276886a14063b331e0f4b1ed19853d2a6
Jonathan Johnson <me@jondavidjohn.com>
2026-06-09 15:53:32 -0500

    fix up some lua nvim config

--- home/.config/nvim/lua/config/plugins/lsp-config.lua
+++ home/.config/nvim/lua/config/plugins/lsp-config.lua
@@ -25,12 +25,37 @@ return {
         filetypes = { "ruby" },
         capabilities = caps,
         root_markers = { "sorbet/config" },
+        root_dir = function(bufnr, on_dir)
+          local root = vim.fs.root(bufnr, "sorbet/config")
+          if root then
+            on_dir(root)
+          end
+        end,
       })
 
       vim.lsp.config('rubocop', {
         cmd = { "bundle", "exec", "rubocop", "--lsp" },
         filetypes = { "ruby" },
         capabilities = caps,
+        root_dir = function(bufnr, on_dir)
+          local root = vim.fs.root(bufnr, { ".git", "Gemfile" })
+          if not root then
+            return
+          end
+
+          if vim.uv.fs_stat(root .. "/.rubocop.yml") then
+            on_dir(root)
+            return
+          end
+
+          local lockfile = root .. "/Gemfile.lock"
+          if vim.uv.fs_stat(lockfile) then
+            local contents = table.concat(vim.fn.readfile(lockfile), "\n")
+            if contents:match("\n    rubocop %(") then
+              on_dir(root)
+            end
+          end
+        end,
       })
 
       vim.lsp.enable({ 'lua_ls', 'solargraph', 'gopls', 'yamlls', 'sorbet', 'rubocop' })
